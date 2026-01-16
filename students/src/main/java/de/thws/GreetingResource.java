1b56ed4b5e57ab212b48cf1aa08f8af51ee53070
Matthias Reining <matthias.reining@outlook.com>
2026-01-16 10:09:17 +0100

    Microprofile OpenAPI

--- students/src/main/java/de/thws/GreetingResource.java
+++ students/src/main/java/de/thws/GreetingResource.java
@@ -1,5 +1,7 @@
 package de.thws;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -14,8 +16,15 @@ public class GreetingResource {
         return "Hello from Quarkus REST from THWS";
     }
 
+    /**
+     * Not a simple 'hello world', just a complex greeting
+     * // failure : DRY (dublicated code comment)
+     * 
+     * @return Hell object
+     */
     @GET
     @Path("complex")
+    @Operation(summary = "Complex Greetings", description = "Not a simple 'hello world', just a complex greeting")
     public Hello complexHello() {
         return new Hello("Hello from Quarkus REST from THWS");
     }
